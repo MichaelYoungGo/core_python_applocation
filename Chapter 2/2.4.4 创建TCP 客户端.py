936254b4c70d5d3fd0a28b5e600cbf2675ad5ca0
MichealYoungGo <ustb_yhb@163.com>
2018-12-14 15:39:08 +0800

    创建TCP客户端

--- Chapter 2/2.4.4 创建TCP 客户端.py
+++ Chapter 2/2.4.4 创建TCP 客户端.py
@@ -3,3 +3,25 @@
 # @Author  : 杨宏兵
 # @File    : 2.4.4 创建TCP 客户端.py
 # @Software: PyCharm
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('>')
+    if not data:
+        break
+    tcpCliSock.send(data.encode('utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
+
